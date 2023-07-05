325746bd34b70bc87c9bc3668db70195d09ea28f
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2023-07-05 16:31:16 +0200

    better ui design

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import ToDoList from "./ToDoList";
+import HeaderToDoApp from "./headerToDoApp";
 
 const LOCAL_STORAGE_TODOS = "todos";
 
@@ -34,13 +35,31 @@ export default function ToDoApp() {
   }
 
   return (
-    <div className="container-sm">
-    <div className="input-group mb-3">
-        <button className="input-group-text" onClick={AddToDo}>Add</button>
-        <input ref={todoName} type="text" className="form-control" placeholder="Add yours todo" aria-label="Username" aria-describedby="basic-addon1"/>
-    </div>
+    <div className="container word-wrap text-wrap">
+      <HeaderToDoApp />
+      <div className="input-group mb-3">
+        <button className="input-group-text" onClick={AddToDo}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-plus"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+        </button>
+        <input
+          ref={todoName}
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Add yours task"
+          aria-describedby="basic-addon1"
+        />
+      </div>
 
-      
+      {todos.length === 0 && <p className="h5 text-center">No quests yet</p>}
       <ToDoList todos={todos} />
     </div>
   );
---
+++ src/headerToDoApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function headerToDoApp() {
+  const title = "Simple to todo app";
+  return (
+    <div>
+      <h1 className="text-center mt-5 mb-5 display-1">{title}</h1>
+    </div>
+  );
+}
