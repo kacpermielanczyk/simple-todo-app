1f495571c7fdf457ffeeed770d8abd6e5a57ea41
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2023-07-05 12:32:32 +0200

    added save and load information to localstorage

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,18 +1,36 @@
-import { React, useState, useRef } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import ToDoList from "./ToDoList";
+
+const LOCAL_STORAGE_TODOS = "todos";
 
 export default function ToDoApp() {
   const [todos, setToDos] = useState([]);
 
   const todoName = useRef();
 
+  // load from localstorage
+  useEffect(() => {
+    const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOS));
+    if (storedToDos) setToDos(storedToDos);
+    console.log(todos)
+  }, []);
+
+  // save to localstorage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_TODOS, JSON.stringify(todos));
+  }, [todos]);
+
+  // save todo array
   function AddToDo(event) {
     let name = todoName.current.value;
 
     if (name === "") return null;
 
     setToDos((prevToDos) => {
-      return [...prevToDos, { id: name, name: name, complete: false }];
+      return [
+        ...prevToDos,
+        { id: crypto.randomUUID(), name: name, complete: false },
+      ];
     });
 
     todoName.current.value = null;
