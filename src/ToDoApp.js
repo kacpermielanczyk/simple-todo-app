e5ca234142d5e6c8ca60eb3380f26a03cf04c17d
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2023-07-05 13:48:43 +0200

    bug to fix

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,18 +1,18 @@
-import { React, useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ToDoList from "./ToDoList";
+import { v4 as uuidv4 } from 'uuid';
 
 const LOCAL_STORAGE_TODOS = "todos";
 
 export default function ToDoApp() {
   const [todos, setToDos] = useState([]);
-
   const todoName = useRef();
 
   // load from localstorage
   useEffect(() => {
     const storedToDos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOS));
     if (storedToDos) setToDos(storedToDos);
-    console.log(todos)
+    console.log(storedToDos)
   }, []);
 
   // save to localstorage
@@ -23,16 +23,17 @@ export default function ToDoApp() {
   // save todo array
   function AddToDo(event) {
     let name = todoName.current.value;
-
     if (name === "") return null;
 
     setToDos((prevToDos) => {
       return [
         ...prevToDos,
-        { id: crypto.randomUUID(), name: name, complete: false },
+        { id: uuidv4(), name: name, complete: false },
       ];
     });
 
+    localStorage.setItem(LOCAL_STORAGE_TODOS, JSON.stringify(todos));
+    
     todoName.current.value = null;
   }
 
